ebbd4ea331a616e0823248d89f11f14f51f07ccc
Ederson <edersonvidal137@gmail.com>
2023-10-23 17:08:42 -0300

    Envio dos arquivos adicionados

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; // Importe um arquivo CSS para estilizar o layout, se necessário
 
-function App() {
+const App = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const playSound = () => {
+    const audio = new Audio(require('./assets/sounds/hector-bell.mp3'));
+    audio.play();
+
+    setIsPlaying(true);
+
+    setTimeout(() => setIsPlaying(false), 100)
+    
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">
+        <img className="logo" src={require("./assets/img/hector.png")} alt="LOOK" />
+        <h1>Hector Bell Button</h1>
+        <img className="logo" src={require("./assets/img/hector.png")} alt="AT ME" />
+      </div>
+
+      <div className="content">
+        <img
+          src={require(isPlaying ? './assets/img/clicked-bell.png' : './assets/img/bell.png')}
+          alt="Imagem"
+          onClick={playSound}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
